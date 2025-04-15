8dfa93a0b6e525cace686de42a93db4f87e4d06f
mohammedadiga <mohammed.adiga011@gmail.com>
2025-04-15 10:52:41 +0200

    chore: initialize React Native project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    Inter: require('@/assets/fonts/Inter.ttf'),
+    Cairo: require('@/assets/fonts/Cairo.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+
+export default function index() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}>
+      <Text style={{ color: 'white', textAlign: 'center', fontSize: 22 }}>Add your story/project in the index.tsx file</Text>
+    </SafeAreaView>
+  );
+}
